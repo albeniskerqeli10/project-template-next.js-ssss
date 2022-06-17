0a7b57df7b46eedc06d9c72aa83d25dcdfde3f21
Albenis Kërqeli <albeniskerqeli5@gmail.com>
2022-06-17 22:26:50 +0000

    Update next.config.js and index.tsx

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
+  images:[
+      "a0.muscache.com"
+  ]
 }
 
 module.exports = nextConfig
